020846d636aa2eccc1ceb0ea9ae62286f58d1e02
YJack0000 <jackleagends@gmail.com>
2023-06-09 12:45:24 +0800

    fix: HE add: presentation new build

--- src/components/FinalProjectSection.vue
+++ src/components/FinalProjectSection.vue
@@ -2,7 +2,45 @@
     <div>
         <div class="mx-auto max-w-screen-sm text-center mb-8 lg:mb-12">
             <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-                Final Prototype
+                Final Presentation
+            </h2>
+        </div>
+        <div
+            style="
+                position: relative;
+                width: 100%;
+                height: 0;
+                padding-top: 56.25%;
+                padding-bottom: 0;
+                box-shadow: 0 2px 8px 0 rgba(63, 69, 81, 0.16);
+                margin-top: 1.6em;
+                margin-bottom: 8vh;
+                overflow: hidden;
+                border-radius: 8px;
+                will-change: transform;
+            "
+        >
+            <iframe
+                loading="lazy"
+                style="
+                    position: absolute;
+                    width: 100%;
+                    height: 100%;
+                    top: 0;
+                    left: 0;
+                    border: none;
+                    padding: 0;
+                    margin: 0;
+                "
+                src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFk10aKOMA&#x2F;view?embed"
+                allowfullscreen="allowfullscreen"
+                allow="fullscreen"
+            >
+            </iframe>
+        </div>
+        <div class="mx-auto max-w-screen-sm text-center mb-8 lg:mb-12">
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+                Hi-Fi Prototype
             </h2>
         </div>
         <div class="w-full">
